a5d34d69dbd9e3cef8512fc39abf3e950eea8ed1
Dave <daizcoding@gmail.com>
2019-07-01 00:17:05 +0300

    fixed app initialization

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
+import asyncio
+
 from aiohttp.web import Application, run_app
 from gino.ext.aiohttp import Gino
 
-from models import Country
+from models import Country, init_db
 from resource import GenericResource
 from settings import DB_ADDRESS
 
 
-db = Gino()
-app = Application(middlewares=[db])
+async def main(loop):
 
-db.init_app(
-    app,
-    config={
-        'dsn': DB_ADDRESS
-    }
-)
+    await init_db()
+    db = Gino()
+    app = Application(middlewares=[db])
 
-countries = GenericResource('countries', Country)
-countries.register(app.router)
+    db.init_app(
+        app,
+        config={
+            'dsn': DB_ADDRESS
+        }
+    )
+
+    countries = GenericResource('countries', Country)
+    countries.register(app.router)
+
+    return app
 
 
 if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    app = loop.run_until_complete(main(loop))
     run_app(app)
