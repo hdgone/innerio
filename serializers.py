f83e86f29b337a42be7fe28210c97001d5372135
Dave <daizcoding@gmail.com>
2019-07-01 00:16:50 +0300

    fixed serializer logic

--- serializers.py
+++ serializers.py
@@ -3,6 +3,10 @@ from collections import OrderedDict
 
 
 class ModelSerializer:
+    """
+    :param query: query set with a fetched model
+    :return json-representation of a given queryset
+    """
     def __init__(self, query):
         self.query = query
 
@@ -11,7 +15,17 @@ class ModelSerializer:
         return json.dumps(data, indent=4).encode('utf-8')
 
     async def serialize_list(self):
-        return OrderedDict(
-            (field, getattr(self.query, field)) for field
-            in self.query.__table__.columns
-        )
+        data = []
+        tablename = self.query[0].__table__.name
+
+        for instance in self.query:
+            od = OrderedDict()
+
+            for field in self.query[0].__table__.columns:
+                # remove table prefix for every field in a table
+                field = str(field).replace(f'{tablename}.', '')
+                od[field] = getattr(instance, field)
+
+            data.append(od)
+
+        return data
